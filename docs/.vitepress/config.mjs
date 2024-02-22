acedbc7aac11190eb17e96787d86873feee7790e
Burak Karakan <burak.karakan@gmail.com>
2024-02-22 15:05:49 +0300

    change base for vitepress

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "ingestr",
   description: "Ingest & copy data between any source and any destination",
+  base: '/ingestr/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
