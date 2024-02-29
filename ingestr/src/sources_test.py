9fa513363eb8bf2d1836b30de0ff16e756c44825
Burak Karakan <burak.karakan@gmail.com>
2024-02-29 23:56:26 +0300

    test mongodb source

--- ingestr/src/sources_test.py
+++ ingestr/src/sources_test.py
@@ -3,7 +3,7 @@ import unittest
 import dlt
 import pytest
 
-from ingestr.src.sources import SqlSource
+from ingestr.src.sources import MongoDbSource, SqlSource
 
 
 class SqlSourceTest(unittest.TestCase):
@@ -50,5 +50,53 @@ class SqlSourceTest(unittest.TestCase):
             return dlt.resource()
 
         source = SqlSource(table_builder=sql_table)
+        res = source.dlt_source(uri, table, incremental_key=incremental_key)
+        self.assertIsNotNone(res)
+
+
+class MongoDbSourceTest(unittest.TestCase):
+    def test_sql_source_requires_two_fields_in_table(self):
+        source = MongoDbSource()
+        with pytest.raises(ValueError):
+            uri = "mongodb://my-project"
+            source.dlt_source(uri, "onetable")
+
+        with pytest.raises(ValueError):
+            uri = "mongodb://my-project"
+            source.dlt_source(uri, "onetable.with.too.many.fields")
+
+    def test_table_instance_is_created(self):
+        uri = "mongodb://my-project"
+        table = "schema.table"
+
+        # monkey patch the mongo function
+        def mongo(connection_url, database, collection, incremental, parallel):
+            self.assertEqual(connection_url, uri)
+            self.assertEqual(database, "schema")
+            self.assertEqual(collection, "table")
+            self.assertIsNone(incremental)
+            self.assertTrue(parallel)
+            return dlt.resource()
+
+        source = MongoDbSource(table_builder=mongo)
+        res = source.dlt_source(uri, table)
+        self.assertIsNotNone(res)
+
+    def test_table_instance_is_created_with_incremental(self):
+        uri = "mongodb://my-project"
+        table = "schema.table"
+        incremental_key = "id"
+
+        # monkey patch the mongo function
+        def mongo(connection_url, database, collection, incremental, parallel):
+            self.assertEqual(connection_url, uri)
+            self.assertEqual(database, "schema")
+            self.assertEqual(collection, "table")
+            self.assertIsInstance(incremental, dlt.sources.incremental)
+            self.assertEqual(incremental.cursor_path, incremental_key)
+            self.assertTrue(parallel)
+            return dlt.resource()
+
+        source = MongoDbSource(table_builder=mongo)
         res = source.dlt_source(uri, table, incremental_key=incremental_key)
         self.assertIsNotNone(res)
