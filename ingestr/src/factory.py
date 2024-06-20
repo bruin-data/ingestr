7008a371e0149fbc7ae4670025d86adf9773e911
Burak Karakan <burak.karakan@gmail.com>
2024-06-20 14:16:53 +0200

    add schema for gorgias tickets

--- ingestr/src/factory.py
+++ ingestr/src/factory.py
@@ -46,6 +46,9 @@ class SourceProtocol(Protocol):
     def dlt_source(self, uri: str, table: str, **kwargs):
         pass
 
+    def handles_incrementality(self) -> bool:
+        pass
+
 
 class DestinationProtocol(Protocol):
     def dlt_dest(self, uri: str, **kwargs) -> Destination:
--- ingestr/src/gorgias/helpers.py
+++ ingestr/src/gorgias/helpers.py
@@ -1,8 +1,9 @@
-"""Shopify source helpers"""
+"""Gorgias source helpers"""
 
-from typing import Any, Iterable, Optional
+import base64
+from typing import Any, Iterable, Optional, Tuple
 
-from dlt.common import jsonpath
+from dlt.common.pendulum import pendulum
 from dlt.common.time import ensure_pendulum_datetime
 from dlt.common.typing import Dict, TDataItems
 from dlt.sources.helpers import requests
@@ -35,7 +36,8 @@ class GorgiasApi:
         self,
         resource: str,
         params: Optional[Dict[str, Any]] = None,
-        latest_updated_at: Optional[str] = None,
+        start_date: Optional[str] = None,
+        end_date: Optional[str] = None,
     ) -> Iterable[TDataItems]:
         """Get all pages from Gorgias using requests.
         Iterates through all pages and yield each page items.
@@ -51,17 +53,14 @@ class GorgiasApi:
         rate = Rate(2, Duration.SECOND)
         limiter = Limiter(rate, raise_when_fail=False)
 
-        latest_updated_at_object = (
-            ensure_pendulum_datetime(latest_updated_at) if latest_updated_at else None
-        )
-
-        print("Received latest update", latest_updated_at_object)
+        start_date_obj = ensure_pendulum_datetime(start_date) if start_date else None
 
         if not params:
             params = {}
 
         params["limit"] = 100
-        params["order_by"] = "updated_datetime:desc"
+        if "order_by" not in params:
+            params["order_by"] = "updated_datetime:desc"
 
         while True:
             limiter.try_acquire(f"gorgias-{self.domain}")
@@ -69,38 +68,83 @@ class GorgiasApi:
                 url, params=params, auth=HTTPBasicAuth(self.email, self.api_key)
             )
             response.raise_for_status()
+            if len(response.json()["data"]) == 0:
+                break
 
             json = response.json()
-            yield [self._convert_datetime_fields(item) for item in json["data"]]
+
+            items = self.__filter_items_in_range(json["data"], start_date, end_date)
+            if len(items) > 0:
+                yield items
+
+            # if there is no cursor, yield the items first and then break the loop
             cursor = json.get("meta", {}).get("next_cursor")
+            params["cursor"] = cursor
             if not cursor:
                 break
 
-            print(f"Fetching next page for {resource} with cursor: {cursor}")
-            if latest_updated_at_object:
-                last_updated_at = jsonpath.find_values(
-                    "data[-1].updated_datetime", json
-                )[0]
-                if latest_updated_at_object >= ensure_pendulum_datetime(
-                    last_updated_at
-                ):
+            if start_date_obj:
+                max_datetime = self.__find_latest_timestamp_from_page(json["data"])
+                if start_date_obj > ensure_pendulum_datetime(max_datetime):
                     break
 
-            params["cursor"] = cursor
+    def __find_latest_timestamp_from_page(
+        self, items: list[Dict[str, Any]]
+    ) -> Optional[Dict[str, Any]]:
+        latest_time = None
+        for item in items:
+            _, max_field_value = self.__get_max_datetime_from_datetime_fields(item)
+            if (
+                not latest_time
+                or ensure_pendulum_datetime(max_field_value) > latest_time
+            ):
+                latest_time = max_field_value
+
+        return latest_time
 
     def _convert_datetime_fields(self, item: Dict[str, Any]) -> Dict[str, Any]:
-        """Convert timestamp fields in the item to pendulum datetime objects
-
-        The item is modified in place.
-
-        Args:
-            item: The item to convert
-
-        Returns:
-            The same data item (for convenience)
-        """
-        fields = ["created_datetime", "updated_datetime"]
-        for field in fields:
-            if field in item:
+        for field in item:
+            if field.endswith("_datetime") and item[field] is not None:
                 item[field] = ensure_pendulum_datetime(item[field])
+
+        if "updated_datetime" not in item:
+            _, max_datetime = self.__get_max_datetime_from_datetime_fields(item)
+            item["updated_datetime"] = max_datetime
+
         return item
+
+    def __filter_items_in_range(
+        self,
+        items: list[Dict[str, Any]],
+        start_date: Optional[str],
+        end_date: Optional[str],
+    ) -> list[Dict[str, Any]]:
+        start_date_obj = ensure_pendulum_datetime(start_date) if start_date else None
+        end_date_obj = ensure_pendulum_datetime(end_date) if end_date else None
+
+        filtered = []
+        for item in items:
+            converted_item = self._convert_datetime_fields(item)
+            if start_date_obj and item["updated_datetime"] < start_date_obj:
+                continue
+            if end_date_obj and item["updated_datetime"] > end_date_obj:
+                continue
+            filtered.append(converted_item)
+
+        return filtered
+
+    def __get_max_datetime_from_datetime_fields(
+        self, item: Dict[str, Any]
+    ) -> Tuple[str, Optional[pendulum.DateTime]]:
+        """Get the maximum datetime from any field that ends with _datetime"""
+
+        max_field_name = None
+        max_field_value = None
+        for field in item:
+            if field.endswith("_datetime") and item[field] is not None:
+                dt = ensure_pendulum_datetime(item[field])
+                if not max_field_name or dt > max_field_value:
+                    max_field_name = field
+                    max_field_value = dt
+
+        return max_field_name, max_field_value
