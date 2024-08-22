8d981e331ab9c5c24a89e18b7c87e3fcb9ccce8a
Sanju Shah <sanjushahgupta@gmail.com>
2024-08-22 11:43:43 +0200

    add messages as resources

--- ingestr/src/sources.py
+++ ingestr/src/sources.py
@@ -405,6 +405,40 @@ class SlackSource:
    def handles_incrementality(self) -> bool:
         return True
    
-   def source_dlt(self, uri: str, table: str, **kwargs):
+   def dlt_source(self, uri: str, table: str, **kwargs):
+        if kwargs.get("incremental_key"):
+            raise ValueError(
+                "Slack takes care of incrementality on its own, you should not provide incremental_key"
+            )
+        #slack://?api_key=<apikey>
+        api_key = None
+        source_field = urlparse(uri)
+        source_query = parse_qs(source_field.query)
+        api_key = source_query.get("api_key")
 
-       return slack_source()
+        if not api_key:
+            raise ValueError ("api_key in the URI is required to connect to Slack")
+
+        endpoint = None
+        msg_channels = None
+        if table in ["channels","users","messages","access_logs"]:
+            endpoint = table
+        elif table.startswith("messages"):
+            channles_part = table.split(":")[1]
+            msg_channels =  channles_part.split(',')
+            endpoint = "messages"
+        else:
+            raise ValueError(f"Resource '{table}' is not supported for slack source yet, if you are interested in it please create a GitHub issue at https://github.com/bruin-data/ingestr")
+
+        date_args ={}
+        if kwargs.get("interval_start"):
+            date_args["start_date"] = kwargs.get("interval_start")
+
+        if kwargs.get("interval_end"):
+            date_args["end_date"] = kwargs.get("interval_end")
+
+        return slack_source(
+            access_token=api_key[0],
+            table_per_channel=False,
+            selected_channels= msg_channels
+        ).with_resources(endpoint)
