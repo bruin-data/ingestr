a698485252e648e97d2b21ac9baec5638707c91a
Sanju Shah <sanjushahgupta@gmail.com>
2024-08-23 15:19:11 +0200

    disable nesting

--- ingestr/src/facebook_ads/__init__.py
+++ ingestr/src/facebook_ads/__init__.py
@@ -50,7 +50,7 @@ from .settings import (
 )
 
 
-@dlt.source(name="facebook_ads")
+@dlt.source(name="facebook_ads", max_table_nesting=0)
 def facebook_ads_source(
     account_id: str = dlt.config.value,
     access_token: str = dlt.secrets.value,
@@ -118,7 +118,7 @@ def facebook_ads_source(
     return campaigns, ads, ad_sets, ad_creatives, ads | leads
 
 
-@dlt.source(name="facebook_ads")
+@dlt.source(name="facebook_ads", max_table_nesting=0)
 def facebook_insights_source(
     account_id: str = dlt.config.value,
     access_token: str = dlt.secrets.value,
