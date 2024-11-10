fb0c2ec88c7edc3e06b8c35004be5af972b5eb2c
Sanju <sanjushahgupta@gmail.com>
2024-11-10 19:34:41 +0100

    implement incremental loading correctly

--- ingestr/src/adjust/_init_.py
+++ ingestr/src/adjust/_init_.py
@@ -2,6 +2,7 @@ from typing import Sequence
 
 import dlt
 from dlt.sources import DltResource
+from dlt.common.time import ensure_pendulum_datetime
 
 from .helpers import DEFAULT_DIMENSIONS, AdjustAPI
 
@@ -12,20 +13,22 @@ def adjust_source(
     end_date: str,
     api_key: str,
 ) -> Sequence[DltResource]:
+    start_date_obj = ensure_pendulum_datetime(start_date)
+   
     @dlt.resource(write_disposition="merge", merge_key="day")
-    def campaigns():
+    def campaigns(updated=dlt.sources.incremental('day', start_date_obj.isoformat())):
         adjust_api = AdjustAPI(api_key=api_key)
         yield from adjust_api.fetch_report_data(
-            start_date=start_date,
+            start_date=updated.start_value,
             end_date=end_date,
         )
 
     @dlt.resource(write_disposition="merge", merge_key="day")
-    def creatives():
+    def creatives(updated=dlt.sources.incremental('day', start_date_obj.isoformat())):
         dimensions = DEFAULT_DIMENSIONS + ["adgroup", "creative"]
         adjust_api = AdjustAPI(api_key=api_key)
         yield from adjust_api.fetch_report_data(
-            start_date=start_date, end_date=end_date, dimensions=dimensions
+            start_date=updated.start_value, end_date=end_date, dimensions=dimensions
         )
 
     return campaigns, creatives
