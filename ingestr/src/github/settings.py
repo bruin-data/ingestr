6432cac1d5c503c57380b1ce458053a9991d5810
Sanju <sanjushahgupta@gmail.com>
2024-11-18 14:50:57 +0100

    remove unused variables

--- ingestr/src/github/settings.py
+++ ingestr/src/github/settings.py
@@ -1,7 +1,5 @@
 """Github source settings and constants."""
 
-START_DATE = "1970-01-01T00:00:00Z"
-
 # rest queries
 REST_API_BASE_URL = "https://api.github.com"
 REPO_EVENTS_PATH = "/repos/%s/%s/events"
