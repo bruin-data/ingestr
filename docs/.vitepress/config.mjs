8a66c806ee3ca5ed55fc72d366285c6ee943585c
turtleDev <saravjeetamansingh@gmail.com>
2024-12-11 15:51:28 +0530

    docs: organise platforms in alphabetical order

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -85,10 +85,10 @@ export default defineConfig({
             text: "Platforms",
             collapsed: false,
             items: [
-              { text: "Asana", link: "/supported-sources/asana.md" },
               { text: "Adjust", link: "/supported-sources/adjust.md" },
               { text: "Airtable", link: "/supported-sources/airtable.md" },
               { text: "AppsFlyer", link: "/supported-sources/appsflyer.md" },
+              { text: "Asana", link: "/supported-sources/asana.md" },
               { text: "Chess.com", link: "/supported-sources/chess.md" },
               {
                 text: "Facebook Ads",
