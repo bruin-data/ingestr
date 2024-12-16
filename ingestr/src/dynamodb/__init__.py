97fb7acacae2cac9f275fb2657cefbe2489c43e8
turtleDev <saravjeetamansingh@gmail.com>
2024-12-16 22:14:22 +0530

    sources: dynamodb: add incremental loading

--- ingestr/src/dynamodb/__init__.py
+++ ingestr/src/dynamodb/__init__.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import dlt
 import boto3
 from boto3.dynamodb.conditions import Attr
@@ -5,31 +6,60 @@ from boto3.dynamodb.conditions import Attr
 from typing import Optional
 from dlt.common.configuration.specs import AwsCredentials
 
+
 PAGINATION_KEY = "LastEvaluatedKey"
 FILTER_KEY = "FilterExpression"
 DATA_KEY = "Items"
 
-@dlt.source
-def dynamodb_source(table: str, credentials: AwsCredentials, incremental: Optional[dlt.sources.incremental] = None):
+@dataclass
+class TableSchema:
+    primary_key: str
+    sort_key: str
+
+def parseSchema(table) -> TableSchema:
+    schema = TableSchema(None, None)
+    for key in table.key_schema:
+        match key["KeyType"]:
+            case "HASH":
+                schema.primary_key = key["AttributeName"]
+            case "RANGE":
+                schema.sort_key = key["AttributeName"]
+
+    if schema.primary_key is None:
+        raise ValueError(f"Table {table.name} has no primary key!")
+
+    return schema
+
+@dlt.source(name="dynamodb")
+def dynamodb_source(
+    table_name: str,
+    credentials: AwsCredentials,
+    incremental: Optional[dlt.sources.incremental] = None
+):
     sesh = boto3.Session(
         aws_access_key_id=credentials.aws_access_key_id,
         aws_secret_access_key=credentials.aws_secret_access_key,
         region_name=credentials.region_name,
     )
     db = sesh.resource("dynamodb")
+    table = db.Table(table_name)
+    schema = parseSchema(table)
+    resource = dlt.resource(
+        dynamodb_table,
+        write_disposition="merge",
+        primary_key=schema.primary_key,
+    )
 
-    # TODO: dynamically bind primary key
-    yield scan_table(db.Table(table), incremental)
+    yield resource(table, incremental)
 
-@dlt.resource(write_disposition="merge")
-def scan_table(table, incremental: Optional[dlt.sources.incremental] = None):
+def dynamodb_table(table, incremental: Optional[dlt.sources.incremental] = None):
     scan_args = {}
     if incremental and incremental.last_value:
         scan_args[FILTER_KEY] =  Attr(incremental.cursor_path).gt(incremental.last_value)
 
     scan = table.scan(**scan_args)
     while True:
-        yield scan[DATA_KEY]
+        yield from scan[DATA_KEY]
         if PAGINATION_KEY not in scan:
             break
         scan = table.scan(ExclusiveStartKey=scan[PAGINATION_KEY], **scan_args)
