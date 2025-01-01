0bad2cfedc16ce72267cea36c548121ef0164cec
Sanju <sanjushahgupta@gmail.com>
2025-01-01 15:03:16 +0100

    fetch pages based on API pagination logic

--- ingestr/src/linkedin_ads/__init__.py
+++ ingestr/src/linkedin_ads/__init__.py
@@ -1,33 +1,8 @@
 
 
-import requests
-
-
-def custom_table(access_token: str, account_id: list[str], start_date: str, end_date: str, dimensions: list[str], metrics: list[str],
-                 time_granularity: str = "DAILY", pivot: str = "CAMPAIGN", fields: str = "pivotValues,dateRange"
-                 ) -> dict:
-   
-    base_url = "https://api.linkedin.com/rest/adAnalytics"
     
-    params = {
-        "q": "analytics",
-        "timeGranularity": time_granularity,
-        "dateRange": f"(start:(year:{start_date['year']},month:{start_date['month']},day:{start_date['day']}))",
-        "accounts": f"List(urn:li:sponsoredAccount:{account_id})",
-        "pivot": pivot,
-        "fields": fields
-    }
     
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Linkedin-Version": "202411",
-        "X-Restli-Protocol-Version": "2.0.0"
-    }
-    
-    response = requests.get(base_url, params=params, headers=headers)
-    response.raise_for_status()
-    
-    return response.json()
+  
 
 
 
--- ingestr/src/linkedin_ads/helpers.py
+++ ingestr/src/linkedin_ads/helpers.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from dlt.sources.helpers.requests import Client
+from datetime import datetime
 
 def retry_on_limit(
     response: requests.Response | None, exception: BaseException | None
@@ -45,3 +46,78 @@ def flat_structure(items, pivot, time_granularity):
     return items
 
 
+class LinkedInAdsAPI:
+    def __init__(
+        self,
+        access_token,
+        time_granularity,
+        account_ids,
+        dimension,
+        metrics,
+        interval_start,
+        interval_end
+    ):
+        self.time_granularity:str = time_granularity
+        self.account_ids:list[str] = account_ids
+        self.dimension:str = dimension
+        self.metrics:list[str] = metrics
+        self.interval_start:str = interval_start
+        self.interval_end:str = interval_end
+        self.headers = {
+             "Authorization": f"Bearer {access_token}",
+            "Linkedin-Version": "202411",
+            "X-Restli-Protocol-Version": "2.0.0"
+        }
+        #interval start is compulsory but end is optional
+        self.start = datetime.strptime(interval_start, "%Y-%m-%d")
+        if interval_end:
+            self.end = datetime.strptime(interval_end, "%Y-%m-%d")
+        else:
+            self.end = None
+    
+
+    def fetch_pages(self):
+        date_range = f"start:(year:{self.start.year},month:{self.start.month},day:{self.start.day})"
+        if self.interval_end:
+            date_range += f",end:(year:{self.end.year},month:{self.end.month},day:{self.end.day})"
+        accounts = ",".join([f"urn:li:sponsoredAccount:{account_id}" for account_id in self.account_ids])
+        
+        params = {
+        "q": "analytics",
+        "timeGranularity": self.time_granularity,
+        "dateRange": date_range,
+        "accounts": f"List({accounts})",
+        "pivot": self.dimension,
+        "fields": self.metrics
+        }
+    
+        base_url = "https://api.linkedin.com/rest/adAnalytics"
+        client = create_client()
+        
+        while base_url:
+            response = client.get(url=base_url, headers=self.headers, params=params)
+
+            result = response.json()
+            print(result)
+            items = result.get("elements", [])
+            
+            if not items:
+                raise ValueError("No items found")
+
+            flat_structure(items=items, pivot=self.dimension, time_granularity=self.time_granularity)
+
+            yield items
+
+            next_link = None
+            for link in result.get("paging", {}).get("links", []):
+                if link.get("rel") == "next":
+                    next_link = link.get("href")
+                    break
+            print(next_link)
+            
+            base_url = next_link
+            params = None
+            if not base_url:
+                break
+
+           
