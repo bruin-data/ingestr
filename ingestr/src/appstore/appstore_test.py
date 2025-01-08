0d986a79bd1ae24494093b49b2b42be29fa997aa
turtleDev <saravjeetamansingh@gmail.com>
2025-01-09 02:48:40 +0530

    appstore: add tests for successful loads

--- ingestr/src/appstore/appstore_test.py
+++ ingestr/src/appstore/appstore_test.py
@@ -1,5 +1,9 @@
+import io
+import gzip
 import pytest
+import requests
 import dlt
+import duckdb
 from dlt.pipeline.exceptions import PipelineStepFailed
 from dlt.extract.exceptions import ResourceExtractionError
 from datetime import datetime
@@ -22,6 +26,15 @@ def has_exception(exception, exc_type):
             return True
         exception = exception.__cause__
     return False
+
+@pytest.fixture
+def app_download_testdata():
+    return """\
+Date\tApp Apple Identifier\tCounts\tProcessing Date\tApp Name\tDownload Type\tApp Version\tDevice\tPlatform Version\tSource Type\tSource Info\tCampaign\tPage Type\tPage Title\tPre-Order\tTerritory
+2025-01-07\t1\t590\t2025-01-08\tAcme Inc\tAuto-update\t4.2.40\tiPhone\tiOS 18.1\tApp Store search\t""\t""\tNo page\tNo page\t""\tFR
+2025-01-07\t1\t16\t2025-01-08\tAcme Inc\tAuto-update\t4.2.40\tiPhone\tiOS 18.1\tApp referrer\tcom.burbn.instagram\t""\tStore sheet\tDefault custom product page\t""\tSG
+2025-01-07\t1\t11\t2025-01-08\tAcme Inc\tAuto-update\t4.2.40\tiPhone\tiOS 18.3\tApp Store search\t""\t""\tNo page\tNo page\t""\tMX
+"""
 
 def test_no_report_instances_found():
     """
@@ -156,3 +169,97 @@ def test_no_such_report():
         dlt.pipeline(destination="duckdb").run(src)
 
     assert has_exception(exc, NoSuchReportError)
+
+
+def test_successful_ingestion(app_download_testdata):
+    client = MagicMock()
+    client.list_analytics_report_requests = MagicMock(
+        return_value=AnalyticsReportRequestsResponse(
+            [
+                ReportRequest(
+                    type="analyticsReportRequests",
+                    id="123",
+                    attributes=ReportRequestAttributes(
+                        accessType="ONGOING",
+                        stoppedDueToInactivity=False
+                    )
+                )
+            ],
+            None,
+            None,
+    ))
+    client.list_analytics_reports = MagicMock(
+        return_value=AnalyticsReportResponse(
+            [
+                Report(
+                    type="analyticsReports",
+                    id="123",
+                    attributes=ReportAttributes(
+                        name="app-downloads-detailed",
+                        category="USER"
+                    )
+                )
+            ],
+            None,
+            None,
+    ))
+
+    client.list_report_instances = MagicMock(
+        return_value=AnalyticsReportInstancesResponse(
+            [
+                ReportInstance(
+                    type="analyticsReportInstances",
+                    id="123",
+                    attributes=ReportInstanceAttributes(
+                        granularity="DAILY",
+                        processingDate="2025-01-01"
+                    )
+                )
+            ],
+            None,
+            None,
+    ))
+
+    client.list_report_segments = MagicMock(
+        return_value=AnalyticsReportSegmentsResponse(
+            [
+                ReportSegment(
+                    type="analyticsReportSegments",
+                    id="123",
+                    attributes=ReportSegmentAttributes(
+                        checksum="checksum-0",
+                        url="http://example.com/report.csv", # we'll monkey patch requests.get to return this file
+                        sizeInBytes=123
+                    )
+                )
+            ],
+            None,
+            None,
+    ))
+
+    src = app_store(
+        client,
+        ["1"],
+    ).with_resources("app-downloads-detailed")
+
+    conn = duckdb.connect()
+    dest = dlt.destinations.duckdb(
+        credentials=conn,
+    )
+
+    with patch("requests.get") as mock_get:
+        res = requests.Response()
+        buffer = io.BytesIO()
+        buffer.mode = "rw"
+        archive = gzip.GzipFile(fileobj=buffer, mode="w")
+        archive.write(app_download_testdata.encode())
+        archive.close()
+        buffer.seek(0)
+        res.status_code = 200
+        res.raw = buffer
+
+        mock_get.return_value = res
+
+        dlt.pipeline(destination=dest).run(src)
+
+    conn.sql("select count(*) from public.app_downloads_detailed").fetchone()[0] == 3
