4ff16fa7bc1a607d54cf7d771acc5d85f8a1b7c4
turtleDev <saravjeetamansingh@gmail.com>
2025-01-22 17:08:10 +0530

    googleads: remove useless resources

--- ingestr/src/google_ads/__init__.py
+++ ingestr/src/google_ads/__init__.py
@@ -29,57 +29,9 @@ def google_ads(
     end_date: Optional[datetime] = None,
 ) -> List[DltResource]:
     return [
-        customers(client=client, customer_id=customer_id),
-        campaigns(client=client, customer_id=customer_id),
-        change_events(client=client, customer_id=customer_id),
-        customer_clients(client=client, customer_id=customer_id),
         asset_report_daily(client=client, customer_id=customer_id, start_date=start_date, end_date=end_date),
         ad_report_daily(client=client, customer_id=customer_id, start_date=start_date, end_date=end_date),
     ]
-
-
-@dlt.resource(write_disposition="replace")
-def customers(client: Resource, customer_id: str) -> Iterator[TDataItem]:
-    # Issues a search request using streaming.
-    ga_service = client.get_service("GoogleAdsService")
-    query = "SELECT customer.id, customer.descriptive_name FROM customer"
-    stream = ga_service.search_stream(customer_id=customer_id, query=query)
-    for batch in stream:
-        for row in batch.results:
-            yield to_dict(row.customer)
-
-
-@dlt.resource(write_disposition="replace")
-def campaigns(client: Resource, customer_id: str) -> Iterator[TDataItem]:
-    # Issues a search request using streaming.
-    ga_service = client.get_service("GoogleAdsService")
-    query = "SELECT campaign.id, campaign.labels FROM campaign"
-    stream = ga_service.search_stream(customer_id=customer_id, query=query)
-    for batch in stream:
-        for row in batch.results:
-            yield to_dict(row.campaign)
-
-
-@dlt.resource(write_disposition="replace")
-def change_events(client: Resource, customer_id: str) -> Iterator[TDataItem]:
-    # Issues a search request using streaming.
-    ga_service = client.get_service("GoogleAdsService")
-    query = "SELECT change_event.change_date_time FROM change_event WHERE change_event.change_date_time during LAST_14_DAYS LIMIT 1000"
-    stream = ga_service.search_stream(customer_id=customer_id, query=query)
-    for batch in stream:
-        for row in batch.results:
-            yield to_dict(row.change_event)
-
-
-@dlt.resource(write_disposition="replace")
-def customer_clients(client: Resource, customer_id: str) -> Iterator[TDataItem]:
-    # Issues a search request using streaming.
-    ga_service = client.get_service("GoogleAdsService")
-    query = "SELECT customer_client.status FROM customer_client"
-    stream = ga_service.search_stream(customer_id=customer_id, query=query)
-    for batch in stream:
-        for row in batch.results:
-            yield to_dict(row.customer_client)
 
 @dlt.resource
 def asset_report_daily(
