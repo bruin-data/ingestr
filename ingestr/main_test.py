131d70dbd6e4d2fb4ce0c463cea35f27951b3a64
Sanju <sanjushahgupta@gmail.com>
2025-01-27 12:46:39 +0100

    update package version to specific version

--- ingestr/main_test.py
+++ ingestr/main_test.py
@@ -472,7 +472,7 @@ class DuckDb:
 POSTGRES_IMAGE = "postgres:16.3-alpine3.20"
 MYSQL8_IMAGE = "mysql:8.4.1"
 MSSQL22_IMAGE = "mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04"
-CLICKHOUSE_IMAGE = "clickhouse/clickhouse-server:latest"
+CLICKHOUSE_IMAGE = "clickhouse/clickhouse-server:24.12"
 
 pgDocker = DockerImage(lambda: PostgresContainer(POSTGRES_IMAGE, driver=None).start())
 clickHouseDocker = ClickhouseDockerImage(
@@ -1360,11 +1360,12 @@ def test_arrow_mmap_to_db_create_replace(dest):
 )
 def test_arrow_mmap_to_db_delete_insert(dest):
     schema = f"testschema_arrow_mmap_del_ins_{get_random_string(5)}"
+    if "clickhouse" in dest.uri:
+       pytest.skip("")
 
     def run_command(df: pd.DataFrame, incremental_key: Optional[str] = None):
         table = pa.Table.from_pandas(df)
-        if "clickhouse" in dest_uri:
-            pytest.skip("")
+
         with tempfile.NamedTemporaryFile(suffix=".arrow", delete=True) as tmp:
             with pa.OSFile(tmp.name, "wb") as f:
                 writer = ipc.new_file(f, table.schema)
