ce58dff520c254b334a8b6a56887ac35d4ee741f
Sanju <sanjushahgupta@gmail.com>
2025-01-31 16:54:25 +0100

    Update http_port based on secure value when http_port is not given by user

--- ingestr/src/destinations.py
+++ ingestr/src/destinations.py
@@ -299,13 +299,15 @@ class ClickhouseDestination:
             raise ValueError(
                 "The TCP port of the ClickHouse server is required to establish a connection."
             )
-
+        
         query_params = parse_qs(parsed_uri.query)
-        http_port = (
-            int(query_params["http_port"][0]) if "http_port" in query_params else 8443
-        )
-
         secure = int(query_params["secure"][0]) if "secure" in query_params else 1
+
+        http_port = (
+            int(query_params["http_port"][0])
+            if "http_port" in query_params
+            else 8443 if secure == 1 else 8123
+        )
 
         if secure not in (0, 1):
             raise ValueError(
