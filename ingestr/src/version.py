5f9219be41e60d03e351619f680281ba47867804
turtleDev <saravjeetamansingh@gmail.com>
2025-02-06 17:07:37 +0530

    lint: fix warnings

--- ingestr/src/version.py
+++ ingestr/src/version.py
@@ -1,5 +1,5 @@
 try:
     from ingestr.src import buildinfo
-    __version__ = buildinfo.version.lstrip("v")
+    __version__ = buildinfo.version.lstrip("v")  # type: ignore
 except ImportError:
     __version__ = "0.0.0-dev"
