3087f7d606dceabf575f1b2a1ec7cea0323c3dfd
turtleDev <saravjeetamansingh@gmail.com>
2025-02-06 17:15:38 +0530

    lint: fix v2

--- ingestr/src/version.py
+++ ingestr/src/version.py
@@ -1,5 +1,6 @@
 try:
-    from ingestr.src import buildinfo
-    __version__ = buildinfo.version.lstrip("v")  # type: ignore
+    from ingestr.src import buildinfo  # type: ignore
+
+    __version__ = buildinfo.version.lstrip("v")
 except ImportError:
     __version__ = "0.0.0-dev"
