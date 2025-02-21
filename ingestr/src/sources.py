f3ff4d7f9796966eee11dd856daf3c7231a2e5a9
Sanju Shah <71315276+sanjushahgupta@users.noreply.github.com>
2025-02-21 15:10:12 +0100

    Enhancement/applovin max (#129)
    * rename table name 
    
    * pass application from source table
    *handle error message

--- ingestr/src/sources.py
+++ ingestr/src/sources.py
@@ -1833,6 +1833,9 @@ class AppLovinSource:
 
 
 class ApplovinMaxSource:
+    #expected uri format: applovinmax://?api_key=<api_key>
+    #expected table format: user_ad_revenue:app_id_1,app_id_2
+
     def handles_incrementality(self) -> bool:
         return True
 
@@ -1843,38 +1846,54 @@ class ApplovinMaxSource:
         api_key = params.get("api_key")
         if api_key is None:
             raise ValueError("api_key is required to connect to AppLovin Max API.")
+        
+        AVAILABLE_TABLES = ["user_ad_revenue"]
 
-        application = params.get("application")
-        if application is None:
-            raise ValueError("application is required to connect to AppLovin Max API.")
+        table_fields = table.split(":")
+        requested_table = table_fields[0]
+
+        if len(table_fields) != 2:
+            raise ValueError(
+                "Invalid table format. Expected format is user_ad_revenue:app_id_1,app_id_2"
+            )
+        
+        if requested_table not in AVAILABLE_TABLES:
+            raise ValueError(
+                f"Table name '{requested_table}' is not supported for AppLovin Max source yet."
+                f"Only '{AVAILABLE_TABLES}' are currently supported. "
+                "If you need additional tables, please create a GitHub issue at "
+                "https://github.com/bruin-data/ingestr"
+            )
+        
+        applications = [i for i in table_fields[1].replace(" ", "").split(",") if i.strip()]
+        if len(applications) == 0:
+            raise ValueError(
+                "At least one application id is required"
+            )
+    
+        if len(applications) != len(set(applications)):
+            raise ValueError(
+                "Application ids must be unique."
+            )
 
         interval_start = kwargs.get("interval_start")
         interval_end = kwargs.get("interval_end")
-
-        if "ad_revenue" in table:
-            table = "ad_revenue"
-        else:
-            raise ValueError(
-                f"Table name '{table}' is not supported for AppLovin Max source yet, if you are interested in it please create a GitHub issue at https://github.com/bruin-data/ingestr"
-            )
 
         now = pendulum.now("UTC")
         default_start = now.subtract(days=30).date()
 
         start_date = (
-            interval_start if interval_start is not None else default_start
-        ).strftime("%Y-%m-%d")
-
-        end_date = (
-            interval_end.strftime("%Y-%m-%d") if interval_end is not None else None
+            interval_start.date() if interval_start is not None else default_start
         )
+
+        end_date = interval_end.date() if interval_end is not None else None
 
         return applovin_max_source(
             start_date=start_date,
             end_date=end_date,
             api_key=api_key[0],
-            application=application[0],
-        ).with_resources(table)
+            applications=applications,
+        ).with_resources(requested_table)
 
 
 class SalesforceSource:
