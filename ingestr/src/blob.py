73ce403fd24a5995c635bfe45cc09dafc611f6f9
turtleDev <saravjeetamansingh@gmail.com>
2025-03-14 14:03:57 +0530

    gcs/s3: accept full uri as --table

--- ingestr/src/blob.py
+++ ingestr/src/blob.py
@@ -1,6 +1,9 @@
 import warnings
 from typing import Tuple, TypeAlias
-from urllib.parse import ParseResult
+from urllib.parse import (
+    ParseResult,
+    urlparse
+)
 
 BucketName: TypeAlias = str
 FileGlob: TypeAlias = str
@@ -14,16 +17,19 @@ def parse_uri(uri: ParseResult, table: str) -> Tuple[BucketName, FileGlob]:
     Supports the following Forms:
     - uri: "gs://"
       table: "bucket-name/file-glob"
+    - uri: "gs://uri-bucket-name" (uri-bucket-name is preferred)
+      table: "gs://table-bucket-name/file-glob"
+    - uri: "gs://"
+      table: "gs://bucket-name/file-glob"
     - uri: gs://bucket-name/file-glob
       table: None
     - uri: "gs://bucket-name"
       table: "file-glob"
 
-    The first form is the prefered method. Other forms are supported
-    for backward compatibility, but discouraged.
+    The first form is the prefered method. Other forms are supported but discouraged.
     """
 
-    table = table.strip()
+    table = urlparse(table.strip())
     host = uri.netloc.strip()
 
     if table == "" or uri.path.strip() != "":
@@ -35,14 +41,12 @@ def parse_uri(uri: ParseResult, table: str) -> Tuple[BucketName, FileGlob]:
         return host, uri.path.lstrip("/")
 
     if host != "":
-        warnings.warn(
-            f"Using the form '{uri.scheme}://bucket-name' is deprecated and will be removed in future versions.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return host, table.lstrip("/")
+        return host, table.path.lstrip("/")
 
-    parts = table.lstrip("/").split("/", maxsplit=1)
+    if table.hostname:
+        return table.hostname, table.path.lstrip("/")
+    
+    parts = table.path.lstrip("/").split("/", maxsplit=1)
     if len(parts) != 2:
         return "", parts[0]
 
