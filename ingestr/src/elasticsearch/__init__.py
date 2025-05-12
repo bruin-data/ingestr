3f3b06a71fc9489f21d0d72e4e0f0288f51fa9e2
Sanju <sanjushahgupta@gmail.com>
2025-05-12 17:22:26 +0200

    fix lint

--- ingestr/src/elasticsearch/__init__.py
+++ ingestr/src/elasticsearch/__init__.py
@@ -1,54 +1,37 @@
 import dlt
+
 from elasticsearch import Elasticsearch
 
+
 @dlt.source
-def elasticsearch_source(
-    connection_url: str,
-    index: str,
-    verify_certs: bool
-):
-   
+def elasticsearch_source(connection_url: str, index: str, verify_certs: bool):
     client = Elasticsearch(connection_url, verify_certs=verify_certs)
 
-    @dlt.resource(
-        primary_key="id",
-        write_disposition="merge"
-    )
-    
+    @dlt.resource(primary_key="id", write_disposition="merge")
     def get_documents():
         body = {"query": {"match_all": {}}}
 
-        page = client.search(
-            index=index,
-            scroll="5m",
-            size=5,
-            body=body
-        )
-        
-        sid = page['_scroll_id']
-        hits = page['hits']['hits']
+        page = client.search(index=index, scroll="5m", size=5, body=body)
+
+        sid = page["_scroll_id"]
+        hits = page["hits"]["hits"]
 
         if not hits:
             return
-        
+
         # fetching first page (via .search)
         for doc in hits:
-            yield {
-                "id": doc["_id"],
-                **doc["_source"]
-            }
-        
+            yield {"id": doc["_id"], **doc["_source"]}
+
         while True:
-            #fetching page 2 and other pages (via .scroll)
+            # fetching page 2 and other pages (via .scroll)
             page = client.scroll(scroll_id=sid, scroll="5m")
-            sid = page['_scroll_id']
-            hits = page['hits']['hits']
+            sid = page["_scroll_id"]
+            hits = page["hits"]["hits"]
             if not hits:
                 break
             for doc in hits:
-                yield {
-                    "id": doc["_id"],
-                    **doc["_source"]
-                }
+                yield {"id": doc["_id"], **doc["_source"]}
         client.clear_scroll(scroll_id=sid)
+
     return get_documents
