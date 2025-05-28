6b5b766e2f93a29251bd1a6f7bc678c3a8464273
Burak Karakan <burak.karakan@gmail.com>
2025-05-28 18:41:24 +0300

    chore: add lowercase table names for stripe endpoints

--- ingestr/src/stripe_analytics/settings.py
+++ ingestr/src/stripe_analytics/settings.py
@@ -2,26 +2,25 @@
 
 # the most popular endpoints
 # Full list of the Stripe API endpoints you can find here: https://stripe.com/docs/api.
-ENDPOINTS = (
-    "Subscription",
-    "Account",
-    "Coupon",
-    "Customer",
-    "Product",
-    "Price",
-    "ShippingRate",
-    "Dispute",
-    "SubscriptionItem",
-    "CheckoutSession",
-)
+ENDPOINTS = {
+    "subscription": "Subscription",
+    "account": "Account",
+    "coupon": "Coupon",
+    "customer": "Customer",
+    "product": "Product",
+    "price": "Price",
+    "shippingrate": "ShippingRate",
+    "dispute": "Dispute",
+    "subscriptionitem": "SubscriptionItem",
+    "checkoutsession": "CheckoutSession",
+}
 # possible incremental endpoints
-INCREMENTAL_ENDPOINTS = (
-    "Event",
-    "Invoice",
-    "BalanceTransaction",
-    "Charge",
-    "CustomerBalanceTransaction",
-    "ApplicationFee",
-    "SetupAttempt",
-    "CreditNote",
-)
+INCREMENTAL_ENDPOINTS = {
+    "event": "Event",
+    "invoice": "Invoice",
+    "balancetransaction": "BalanceTransaction",
+    "charge": "Charge",
+    "applicationfee": "ApplicationFee",
+    "setupattempt": "SetupAttempt",
+    "creditnote": "CreditNote",
+}
