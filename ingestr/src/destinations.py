aad04f34fc8743a82d6f76118c72d9a3f1166ba4
Sanju <sanjushahgupta@gmail.com>
2025-06-04 13:45:29 +0200

    add sqlite as a destination

--- ingestr/src/destinations.py
+++ ingestr/src/destinations.py
@@ -468,3 +468,14 @@ class S3Destination:
 
     def post_load(self) -> None:
         pass
+
+
+class SqliteDestination(GenericSqlDestination):
+    def dlt_dest(self, uri: str, **kwargs):
+        return dlt.destinations.sqlalchemy(credentials=uri)
+
+    def dlt_run_params(self, uri: str, table: str, **kwargs):
+        return {
+            "dataset_name": "main",
+            "table_name": table,
+        }
