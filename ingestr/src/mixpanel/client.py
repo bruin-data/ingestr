551690b5cac11abf31124e0a1e55faf553a77875
Sanju <sanjushahgupta@gmail.com>
2025-06-11 14:13:20 +0200

    replace deprecated API secret with username/password auth

--- ingestr/src/mixpanel/client.py
+++ ingestr/src/mixpanel/client.py
@@ -7,19 +7,27 @@ from dlt.sources.helpers.requests import Client
 
 
 class MixpanelClient:
-    def __init__(self, api_secret: str, project_id: str):
-        self.api_secret = api_secret
+    def __init__(self, username: str, password: str, project_id: str):
+        self.username = username
+        self.password = password
         self.project_id = project_id
         self.session = Client(raise_for_status=False).session
 
     def fetch_events(self, start_date: pendulum.DateTime, end_date: pendulum.DateTime) -> Iterable[dict]:
-        url = "https://data.mixpanel.com/api/2.0/export/"
+        url = "https://data-eu.mixpanel.com/api/2.0/export/"
         params = {
             "project_id": self.project_id,
-            "from_date": start_date.format("YYYY-MM-DD"),
-            "to_date": end_date.format("YYYY-MM-DD"),
+            "from_date": start_date.format("2025-06-09"),
+            "to_date": end_date.format("2025-06-11"),
         }
-        resp = self.session.get(url, params=params, auth=(self.api_secret, ""))
+        headers = {
+            "accept": "text/plain",
+        }
+        from requests.auth import HTTPBasicAuth
+        auth = HTTPBasicAuth(self.username, self.password)
+        resp = self.session.get(url, params=params, headers=headers, auth=auth)
+        print("resp", resp.text)
+        print("resp", resp)
         resp.raise_for_status()
         for line in resp.iter_lines():
             if line:
@@ -30,7 +38,7 @@ class MixpanelClient:
         page = 0
         session_id = None
         while True:
-            params = {"project_id": self.project_id, "page": page}
+            params = {"page": page}
             if session_id:
                 params["session_id"] = session_id
             if last_seen is not None:
