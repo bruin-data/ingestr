2b504ab5fc7f65ecfb43d4ebfaa2e4d8595728d7
Sanju <sanjushahgupta@gmail.com>
2025-06-12 07:43:35 +0200

    correct api endpoint

--- ingestr/src/mixpanel/client.py
+++ ingestr/src/mixpanel/client.py
@@ -40,23 +40,37 @@ class MixpanelClient:
     def fetch_profiles(
         self, last_seen: Optional[pendulum.DateTime] = None
     ) -> Iterable[dict]:
-        url = "https://mixpanel.com/api/2.0/engage"
+        url = "https://eu.mixpanel.com/api/query/engage"
+        
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/x-www-form-urlencoded",
+        }
+        from requests.auth import HTTPBasicAuth
+        auth = HTTPBasicAuth(self.username, self.password)
         page = 0
         session_id = None
         while True:
-            params = {"page": page}
+            params = {"project_id": self.project_id, "page": page }
             if session_id:
                 params["session_id"] = session_id
-            if last_seen is not None:
-                where = f'properties["$last_seen"] >= "{last_seen.to_date_string()}"'
-                params["where"] = where
-            resp = self.session.get(url, params=params, auth=(self.api_secret, ""))
+            # if last_seen is not None:
+                # where = f'properties["$last_seen"] >= "{last_seen.to_date_string()}"'
+                # params["where"] = where
+            resp = self.session.post(url, params=params, headers=headers, auth=auth)
+            
             resp.raise_for_status()
             data = resp.json()
             results = data.get("results", [])
+            for result in results:
+                if result["$properties"]:
+                    result["last_seen"] = result["$properties"]["$last_seen"]
+                    result["distinct_id"] = result["$distinct_id"]
+                    
             if not results:
                 break
             session_id = data.get("session_id", session_id)
             for item in results:
                 yield item
             page += 1
+
