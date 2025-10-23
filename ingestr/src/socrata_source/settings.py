4cf17150428c12ab93391214b84dd54974764fb7
Mustafa Ersan <mustafacihatersan@192.168.1.13>
2025-10-23 23:48:03 +0300

    add settings

---
+++ ingestr/src/socrata_source/settings.py
@@ -0,0 +1,8 @@
+"""Socrata API settings and constants"""
+
+# Request timeout in seconds
+REQUEST_TIMEOUT = 30
+
+# Maximum number of records to fetch per page
+# Socrata API supports up to 50000 records per request
+DEFAULT_PAGE_SIZE = 50000
