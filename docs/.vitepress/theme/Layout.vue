42e0b3fa7a0e354b83a0fa7ac155f3cbecd9c8ec
Burak Karakan <burak.karakan@gmail.com>
2025-12-24 17:59:23 +0300

    add a new copy markdown button to docs (#463)

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,27 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import CopyPageButton from './CopyPageButton.vue'
+import { useData } from 'vitepress'
+
+const { Layout } = DefaultTheme
+const { frontmatter } = useData()
+</script>
+
+<template>
+  <Layout>
+    <template #doc-before>
+      <div class="copy-page-wrapper" v-if="frontmatter.layout !== 'home'">
+        <CopyPageButton />
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<style>
+.copy-page-wrapper {
+  float: right;
+  margin-top: 4px;
+  position: relative;
+  z-index: 10;
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,9 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import Layout from './Layout.vue'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout
+}
