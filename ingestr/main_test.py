d12483b4654dda5c04aa0b86d95d187759ddbf75
Burak Karakan <burak.karakan@gmail.com>
2025-12-31 15:32:34 +0300

    add tests for linkedin

--- ingestr/main_test.py
+++ ingestr/main_test.py
@@ -6428,3 +6428,45 @@ def test_snapchat_ads_merge_strategy():
             shutil.rmtree(get_abs_path("../pipeline_data"))
         except Exception:
             pass
+
+
+@pytest.mark.parametrize(
+    "linkedin_ads_table",
+    [
+        "ad_accounts",
+        "ad_account_users",
+        "campaign_groups",
+        "campaigns",
+        "creatives",
+        "conversions",
+        "lead_forms",
+    ],
+)
+def test_linkedin_ads_source_full_refresh(linkedin_ads_table):
+    api_key = os.environ.get("INGESTR_TEST_LINKEDIN_ADS_ACCESS_TOKEN")
+    if not api_key:
+        pytest.skip("INGESTR_TEST_LINKEDIN_ADS_ACCESS_TOKEN not set")
+
+    dbname = f"test_linkedin_{linkedin_ads_table}_{get_random_string(5)}.db"
+    abs_db_path = get_abs_path(f"./testdata/{dbname}")
+    rel_db_path_to_command = f"ingestr/testdata/{dbname}"
+    uri = f"duckdb:///{rel_db_path_to_command}"
+
+    result = invoke_ingest_command(
+        f"linkedinads://?access_token={api_key}",
+        linkedin_ads_table,
+        uri,
+        f"raw.{linkedin_ads_table}",
+    )
+
+    assert result.exit_code == 0
+
+    conn = duckdb.connect(abs_db_path)
+    result = conn.sql(f"select count(*) from raw.{linkedin_ads_table}").fetchone()
+    assert result is not None
+
+    conn.close()
+    try:
+        os.remove(abs_db_path)
+    except Exception:
+        pass
