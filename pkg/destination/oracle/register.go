9763989133b99ac2add7a28c727fd80ff77fb889
Burak Karakan <burak.karakan@gmail.com>
2026-08-05 21:11:10 +0300

    add Oracle Exadata destination support

--- pkg/destination/oracle/register.go
+++ pkg/destination/oracle/register.go
@@ -4,7 +4,7 @@ import "github.com/bruin-data/ingestr/internal/registry"
 
 func init() {
 	registry.RegisterDestination(
-		[]string{"oracle", "oracle+cx_oracle"},
+		[]string{"oracle", "oracle+cx_oracle", "exadata", "oracle+exadata", "oracle_exadata"},
 		func() interface{} { return NewOracleDestination() },
 	)
 }
